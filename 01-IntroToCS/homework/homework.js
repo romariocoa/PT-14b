e08e4bba2e1f84d86f707846bfeae5f62f9fc485
Romario <romarito.7@gmail.com>
2023-05-30 17:19:46 -0500

    subo la homework de introtocs

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,21 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
 
-function DecimalABinario(num) {}
+   let decimal = 0;
+
+   for (let i = 0; i < num.length; i++) {
+
+      decimal += (2 ** i) * parseInt(num.charAt(num.length - 1 - i), 2);;
+
+   }
+   return decimal;
+}
+
+function DecimalABinario(num) {
+   let binario = num.toString(2);
+   return binario;
+}
 
 module.exports = {
    BinarioADecimal,
